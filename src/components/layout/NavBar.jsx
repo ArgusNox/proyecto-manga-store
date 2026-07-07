f07488af5e3dcd7ab563349c1ffcbe494e44ef9f
Argo <87100999+ArgusNox@users.noreply.github.com>
2026-07-06 21:00:11 -0300

    feat: add auth links to navbar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faBookOpen } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faBookOpen, faUserGear, faRightToBracket, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import CartWidget from '../CartWidget/CartWidget'
+import { useAuth } from '../../context/AuthContext'
 
 function NavBar() {
+  const { isAuthenticated, logout } = useAuth()
+
   return (
     <nav className="site-nav">
       <Link to="/">
@@ -13,6 +16,22 @@ function NavBar() {
       <Link to="/productos">
         <FontAwesomeIcon icon={faBookOpen} /> Catálogo
       </Link>
+
+      {isAuthenticated && (
+        <Link to="/admin/productos">
+          <FontAwesomeIcon icon={faUserGear} /> Admin
+        </Link>
+      )}
+
+      {isAuthenticated ? (
+        <button className="nav-button" type="button" onClick={logout}>
+          <FontAwesomeIcon icon={faRightFromBracket} /> Salir
+        </button>
+      ) : (
+        <Link to="/login">
+          <FontAwesomeIcon icon={faRightToBracket} /> Login
+        </Link>
+      )}
 
       <CartWidget />
     </nav>
